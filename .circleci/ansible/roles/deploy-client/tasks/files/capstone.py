eaa7beeabd66158bdf0dd3d9b7a76b2974848508
rjluzardo <rjlsyscripciones@gmail.com>
2021-07-12 09:18:17 -0500

    fixing python script

--- .circleci/ansible/roles/deploy-client/tasks/files/capstone.py
+++ .circleci/ansible/roles/deploy-client/tasks/files/capstone.py
@@ -8,16 +8,16 @@ date_time = today.strftime("%m/%d/%Y, %H:%M:%S")
 def is_hellow_up(date_time):
     try:
         response = requests.get('http://localhost')
-        print('Server up', date_time)
-        print('HTTP Code:',response.status_code)
+        #print('Server up', date_time)
+        #print('HTTP Code:',response.status_code)
         log = open\
             ('/Users/i859241/Dropbox/Github/udacity/udacity_capstone/.circleci/ansible/roles/deploy-client/tasks/files/\
             log.txt', 'a')
         log.write(f'{date_time} Hellow World UP')
         log.close()
     except:
-        print(date_time)
-        print('Server down')
+        ##print(date_time)
+        #print('Server down')
         log = open\
             ('/Users/i859241/Dropbox/Github/udacity/udacity_capstone/.circleci/ansible/roles/deploy-client/tasks/files/\
             log.txt', 'a')
