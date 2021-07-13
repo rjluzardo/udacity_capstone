8b64d7c76615893dbafa9048de918579291bace9
rjluzardo <rjlsyscripciones@gmail.com>
2021-07-13 14:43:47 -0500

    adding deploy-server

--- .circleci/files/run_docker.sh
+++ .circleci/files/run_docker.sh
@@ -3,12 +3,12 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build -t demolocal .
-docker build --tag=rluzardo/app .
-docker push rluzardo/app
+docker build -t uda_cap_hello .
+docker build --tag=rluzardo/uda_cap_hello .
+docker push rluzardo/uda_cap_hello
 
 # Step 2: 
-docker image ls
+#docker image ls
 
 # Step 3: 
-docker run -p 8000:80 demolocal
+docker run -p 8000:80 uda_cap_hello
